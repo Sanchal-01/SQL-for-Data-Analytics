24786458353b31e02b07cd591a0d4eb8552920a6
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-02-25 15:47:23 +0530

    Add SQL queries for retrieving customer and order data
    Initial SELECT statements to retrieve customer and order data.

---
+++ 01_SELECT/01_Basic Select.sql
@@ -0,0 +1,27 @@
+/* ============================================================================
+   SELECT ALL COLUMNS
+=============================================================================== */
+-- Business Question_1: Retrieve All Customer Data
+SELECT *
+FROM customers
+
+-- Business Question_2: Retrieve All Order Data
+SELECT *
+FROM orders 
+
+/* ============================================================================
+   SELECT FEW COLUMNS
+=============================================================================== */
+-- Business Question_1:Retrieve each customer's name, country, and score.
+SELECT 
+  first_name,
+  country,
+  score
+FROM customers
+
+
+
+
+
+
+
