b85a9a6f21199a1a6601b90718d5cc5f361d5c1b
Sanchal Kumar <sanchalkumar124680@gmail.com>
2026-03-17 21:56:06 +0530

    Create 03_DROP.sql

---
+++ 02_DDL/03_DROP.sql
@@ -0,0 +1,6 @@
+/* =============================================
+                     DROP
+================================================ */
+
+--TASK: Delete the table persons from the database
+DROP TABLE persons
